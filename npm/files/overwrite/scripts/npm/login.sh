a86817c70cc60f47dde1032081b0e578cf039b1b
Yongcong Ma <yongcong.ma@neuralgalaxy.com>
2022-04-12 21:02:00 +0800

    fix openapi/npm login

--- npm/files/overwrite/scripts/npm/login.sh
+++ npm/files/overwrite/scripts/npm/login.sh
@@ -10,19 +10,24 @@ echo "Login NPM Registry https://${NGIQ_NPM_REGISTRY_HOST}/${NGIQ_NPM_REGISTRY_U
 
 password=$(aws ssm get-parameter --name /common/cicd/jenkins/${username}_password --with-decryption | jq -r .Parameter.Value)
 
-token=$(curl -s \
-  -H "Accept: application/json" \
-  -H "Content-Type:application/json" \
-  -X PUT --data "{\"name\": \"${NGIQ_NPM_USERNAME}\", \"password\": \"${password}\"}" \
-  https://${NGIQ_NPM_REGISTRY_HOST}/${NGIQ_NPM_REGISTRY_URI}/-/user/org.couchdb.user:${NGIQ_NPM_USERNAME} | jq -r .token)
+# token=$(curl -s \
+#   -H "Accept: application/json" \
+#   -H "Content-Type:application/json" \
+#   -X PUT --data "{\"name\": \"${NGIQ_NPM_USERNAME}\", \"password\": \"${password}\"}" \
+#   https://${NGIQ_NPM_REGISTRY_HOST}/${NGIQ_NPM_REGISTRY_URI}/-/user/org.couchdb.user:${NGIQ_NPM_USERNAME} | jq -r .token)
 
 # .npmrc
 # @ngiq:registry=https://registry.neuralgalaxy.net/repository/npm-group
 # //registry.neuralgalaxy.net/repository/:_authToken=NpmToken.xxxx
 
-rm -rf .npmrc
-echo -e "@ngiq:registry=https://${NGIQ_NPM_REGISTRY_HOST}/${NGIQ_NPM_REGISTRY_URI}" >> .npmrc
-echo -e "//${NGIQ_NPM_REGISTRY_HOST}/:_authToken=${token}" >> .npmrc
+# rm -rf .npmrc
+# echo -e "@ngiq:registry=https://${NGIQ_NPM_REGISTRY_HOST}/${NGIQ_NPM_REGISTRY_URI}" >> .npmrc
+# echo -e "//${NGIQ_NPM_REGISTRY_HOST}/:_authToken=${token}" >> .npmrc
+
+npx npm-cli-login \
+  -u ${NGIQ_NPM_USERNAME} -p ${password} \
+  -e ${NGIQ_NPM_USERNAME}@neuralgalaxy.com \
+  -r https://${NGIQ_NPM_REGISTRY_HOST}/${NGIQ_NPM_REGISTRY_URI}
 
 echo "Login NPM Registry succeeded"
 
--- openapi/index.js
+++ openapi/index.js
@@ -1,43 +1,47 @@
-const { chmodAllSync, copyAllFiles } = require('../util');
+const { chmodAllSync, copyAllFiles, getConfig, isService } = require('../util');
 const { packageJson } = require('../core');
 
-function task() {
-  copyAllFiles(__dirname);
+function task(config) {
+  const type = getConfig(config, 'type', 'service'); // service/lib
 
-  chmodAllSync('./scripts', '755');
+  if (isService(type)) {
+    copyAllFiles(__dirname);
 
-  packageJson()
-    // .setScript('compile:genapicode', 'scripty')
-    .setScript('openapi:codegen', 'scripty')
-    .setScript('openapi:dev', 'scripty')
-    .addDependency('@types/body-parser')
-    .addDependency('@types/ejs')
-    .addDependency('@types/express')
-    .addDependency('@types/joi')
-    .addDependency('@types/lodash')
-    .addDependency('@types/mz')
-    .addDependency('@types/shelljs')
-    .addDependency('@types/helmet')
-    .addDependency('@types/hapi__joi')
-    .addDependency('@hapi/joi', { type: 'product' })
-    .addDependency('body-parser', { type: 'product' })
-    .addDependency('ejs', { type: 'product' })
-    .addDependency('express', { type: 'product' })
-    .addDependency('helmet', { type: 'product' })
-    // .addDependency('joi', { type: 'product' })
-    .addDependency('json-schema')
-    .addDependency('json-schema-ref-parser')
-    .addDependency('json-schema-to-joi')
-    .addDependency('json-schema-to-typescript')
-    .addDependency('lodash', { type: 'product' })
-    .addDependency('mz')
-    .addDependency('openapi-types')
-    .addDependency('shelljs')
-    .addDependency('swagger-parser')
-    .addDependency('ts-node')
-    .addDependency('scripty')
-    .addDependency('@ngiq/nodejs-common', { type: 'product' })
-    .save();
+    chmodAllSync('./scripts', '755');
+
+    packageJson()
+      .setScript('compile:genapicode', 'scripty')
+      .setScript('openapi:codegen', 'scripty')
+      .setScript('openapi:dev', 'scripty')
+      .addDependency('@types/body-parser')
+      .addDependency('@types/ejs')
+      .addDependency('@types/express')
+      .addDependency('@types/joi')
+      .addDependency('@types/lodash')
+      .addDependency('@types/mz')
+      .addDependency('@types/shelljs')
+      .addDependency('@types/helmet')
+      .addDependency('@types/hapi__joi')
+      .addDependency('@hapi/joi', { type: 'product' })
+      .addDependency('body-parser', { type: 'product' })
+      .addDependency('ejs', { type: 'product' })
+      .addDependency('express', { type: 'product' })
+      .addDependency('helmet', { type: 'product' })
+      // .addDependency('joi', { type: 'product' })
+      .addDependency('json-schema')
+      .addDependency('json-schema-ref-parser')
+      .addDependency('json-schema-to-joi')
+      .addDependency('json-schema-to-typescript')
+      .addDependency('lodash', { type: 'product' })
+      .addDependency('mz')
+      .addDependency('openapi-types')
+      .addDependency('shelljs')
+      .addDependency('swagger-parser')
+      .addDependency('ts-node')
+      .addDependency('scripty')
+      .addDependency('@ngiq/nodejs-common', { type: 'product' })
+      .save();
+  }
 }
 
 task.description = 'Generate TypeScript interfaces/Joi schemas code from OpenAPI docs';
